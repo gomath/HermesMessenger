3d527942526e1d6b215f4e699f967e9f3d77a84c
Daniel DiSanto <disanto@mit.edu>
2013-05-03 16:53:55 -0400

    Message and UserInfo classes

--- src/client/Message.java
+++ src/client/Message.java
@@ -1,5 +1,40 @@
 package client;
 
+/**
+ * Object representing an instant message
+ *
+ */
 public class Message {
-
+    private final UserInfo sender;
+    private final Conversation convo;
+    private final String text;
+    public Message(UserInfo sender, Conversation convo, String text) {
+        this.sender = sender;
+        this.convo = convo;
+        this.text = text;
+    }
+    
+    /**
+     * Gets the sender of the message
+     * @return the sender (UserInfo)
+     */
+    public UserInfo getSender() {
+        return this.sender;
+    }
+    
+    /**
+     * Gets the conversation this message is part of
+     * @return the conversation
+     */
+    public Conversation getConvo() {
+        return this.convo;
+    }
+    
+    /**
+     * Gets the text of the message
+     * @return the text
+     */
+    public String getText() {
+        return this.text;
+    }
 }
--- src/client/UserInfo.java
+++ src/client/UserInfo.java
@@ -1,5 +1,59 @@
 package client;
 
-public class UserInfo {
+import java.awt.Color;
+import java.net.Socket;
 
+/**
+ * Object to hold information about other users
+ *
+ */
+public class UserInfo {
+    private final String username;
+    private final Color color;
+    private final Socket socket;
+    /**
+     * Constructor for use by the server to hold information about
+     * connected clients, including socket where they can be contacted.
+     * @param username the username of the connected user
+     * @param color the color associated with that user
+     * @param socket the socket where they can be reached
+     */
+    public UserInfo(String username, Color color, Socket socket) {
+        this.username = username;
+        this.color = color;
+        this.socket = socket;
+    }
+    /**
+     * Constructor for use of other clients to keep track of other
+     * connected users
+     * @param username the username of the user.
+     * @param color the color associated with that user
+     */
+    public UserInfo(String username, Color color) {
+        this.username = username;
+        this.color = color;
+        this.socket = null;
+    }
+    /**
+     * Gets the username for this user
+     * @return the username
+     */
+    public String getUsername() {
+        return this.username;
+    }
+    /**
+     * Gets the color for this user
+     * @return the color
+     */
+    public Color getColor() {
+        return this.color;
+    }
+    /**
+     * Gets the socket where this user can be reached
+     * @return socket where user is connected, or null if called
+     * from another client (which doesn't have that information)
+     */
+    public Socket getSocket() {
+        return this.socket;
+    }
 }
