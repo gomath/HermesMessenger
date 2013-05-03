7c26ce31d7c2c2c6c9fb6b9f8d32e2873ab3ce26
Jenn <jtilton@mit.edu>
2013-05-03 16:54:35 -0400

    Conversation done and User started

--- src/client/Conversation.java
+++ src/client/Conversation.java
@@ -1,5 +1,70 @@
 package client;
 
-public class Conversation {
+import java.util.Collections;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.concurrent.ConcurrentHashMap;
 
+public class Conversation {
+    private final String convoID;
+    private final ConcurrentHashMap<String, UserInfo> participants;
+    private ArrayList<Message> history;
+    
+    /**
+     * Construct a conversation object based on information about
+     * its participants.
+     * @param participants a map from the String usernames of the participants
+     * to their corresponding UserInfo objects.
+     */
+    public Conversation(ConcurrentHashMap<String, UserInfo> participants){
+        this.convoID = alphabetizeHashMap(participants);
+        this.participants = participants;
+        this.history = new ArrayList<Message>();
+    }
+    
+    /**
+     * Add a message to the conversation history.
+     * @param msg the Message to be added
+     */
+    public void addMessage(Message msg){
+        this.history.add(msg);
+    }
+    
+    /**
+     * Getter methods for various private instance variables.
+     * @return the instance variable.
+     */
+    public List<Message> getMessages(){
+        return this.history;
+    }
+    
+    public String getConvoID(){
+        return this.convoID;
+    }
+    
+    public ConcurrentHashMap<String, UserInfo> getParticipantsMap(){
+        return this.participants;
+    }
+    
+    /**
+     * Alphabetize the participants of a Conversation to get their
+     * convoID
+     * @param participants, a HashMap from String usernames to corresponding
+     * UserInfo objects. 
+     * @return a String that is an alphabetized list of the usernames,
+     * with spaces in between each.
+     */
+    public String alphabetizeHashMap(ConcurrentHashMap<String, UserInfo> participants){
+        List<String> userList = new ArrayList<String>();
+        for(String name : participants.keySet()){
+            userList.add(name);
+        }
+        java.util.Collections.sort(userList); //alphabetize
+        StringBuilder convoID = new StringBuilder();
+        for(String userName : userList){
+            convoID.append(userName);
+            convoID.append(' ');
+        }
+        return new String(convoID.substring(0,convoID.length()-1)); //cut off last space
+    }
 }
--- src/client/User.java
+++ src/client/User.java
@@ -1,5 +1,24 @@
 package client;
 
-public class User {
+import java.awt.Color;
+import java.net.Socket;
+import java.util.concurrent.ConcurrentHashMap;
 
+public class User {
+    private final String username;
+    private final Color color;
+    private final Socket socket;
+    private ConcurrentHashMap<String, UserInfo> onlineUsers;
+    private Conversation activeConvo;
+    private ConcurrentHashMap<String, Conversation> myConvos;
+    
+    public User(String username, Color color, Socket socket){
+        this.username = username;
+        this.color = color;
+        this.socket = socket;
+        this.onlineUsers = new ConcurrentHashMap<String, UserInfo>();
+        this.myConvos = new ConcurrentHashMap<String, Conversation>();
+    }
+    
+    
 }
