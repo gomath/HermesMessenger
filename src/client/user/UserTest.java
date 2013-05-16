485ce2b5787c1f95bf2a849a2af79ef837cf29f5
Daniel DiSanto <disanto@mit.edu>
2013-05-16 00:11:01 -0400

    UserTests donezo

--- src/client/user/UserTest.java
+++ src/client/user/UserTest.java
@@ -2,67 +2,126 @@ package client.user;
 
 import static org.junit.Assert.*;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.Socket;
+import java.net.SocketException;
+import java.util.concurrent.ConcurrentHashMap;
+
+import javax.sound.sampled.Line;
 
 import org.junit.Test;
 
 public class UserTest {
+    //Tests that a user is created properly
     @Test
-    public void test() {
-        String username1 = "jenn";
-        String color1 = "orange";
+    public void usernameColorSocketTest() {
+        String username1 = "Banana";
+        String color1 = "yellow";
         Socket socket1 = new Socket();
         User user = new User(username1, color1, socket1);
         assertEquals(user.getUsername(), username1);
         assertEquals(user.getColor(), color1);
         assertEquals(user.getSocket(), socket1);   
-        }
     }
-   // @Test
-   // public void 
     
-/*
- 
-Method: handleRequest(String input)
-Strategy: partition the input space and test all possible inputs that do not affect the GUI and use Integration testing with ConversationView
-Method: sendMessageToServer(String text)
-Unit test by having something listening to a mock socket output
-Method: startConvo(Object[] usernames) 
-Strategy: unit test that tests the correct message is returned when making a convo from username objects
-Method: closeConvo(Conversation convo)
-Strategy: unit test that the right message is sent from the socket given a conversation
-Method: updateConvo(String input)
-Strategy: this involves updating the ConversationView so must be Integration tested with ConversationView
-Method: login()
-Strategy: make sure appropriate message sent to socket outputstream 
-Method: quit()
-Strategy: since this accesses the UserGUI, shoiuld be tested with Integration tests with the GUI
-Method: setActiveConvo(Conversation convo)
-Strategy: make sure the active convo is set to the correct Conversation
-Method: setOnlineUsers(ConcurrentHashMap<String,UserInfo> userMap)
-Strategy: make sure correct users are set
-Method: addOnlineUser(UserInfo user)
-Strategy: ensure users added correctly
-Method: removeOnlineUser(String user)
-Strategy: test that user can be removed from online user list properly
-Method: getUsername()
-Strategy: assert correct username returned
-Method: getColor()
-Strategy: assert correct color returned
-Method: getSocket()
-Strategy: assert correct socket instance returned
-Method: getOnlineUsers()
-Strategy: assert that online users returned
-Method: getMyConvos()
-Strategy: assert correct conversations returned
-Method: addNewMyConvo(Conversation convo)
-Strategy: since this accesses ConversationView needs to be Integration tested with ConversationView
-Method: removeMyConvo(Conversation convo)
-Strategy: make sure correct conversation removed from convos
-Method: checkDuplicateConvo(Conversation convo)
-Strategy: unit test to test expected DuplicateConvoException and to test unique conversation passes (partitioning input space)
-
- */
-
-
-
+    /**
+     * HANDLE CONNECTION
+     * Can't be tested with unit tests because of required socket connection.
+     * Has been visually verified to work because the connection between Client
+     * and Server is working, and all of the incoming messages from the server
+     * are being processed and making the appropriate changes in the GUI.
+     */
+    
+    /**
+     * HANDLE REQUEST
+     * Also can't be unit tested because of the required connection to a server.
+     * Visually tested by manually stepping through actions to induce all possible
+     * messages from the server (listed in the Client/Server protocol documentation).
+     * These incoming messages were successfully received (verified through print statements),
+     * and were processed correctly, making the desired changes in the GUI.
+     */
+    
+    /**
+     * SEND MESSAGE TO SERVER
+     * Can't be unit tested without a server. Visually tested by printing incoming
+     * messages on the server. Actions on the client are properly composed into outgoing
+     * messages and are successfully received by the server.
+     */
+    
+    /**
+     * START CONVO
+     * Can't be unit tested without a server. Visually tested by successfully initializing
+     * conversations, and printing MyConvos, which keeps track of all of the users active
+     * conversations.
+     */
+    
+    /**
+     * CLOSE CONVO
+     * Can't be unit tested without a server. Visually tested by successfully closing
+     * conversations, and printing MyConvos, which keeps track of all of the users active
+     * conversations.
+     */
+    
+    /**
+     * ADD MSG TO CONVO
+     * Can't be tested without a server. Visually tested by sending a message to another user.
+     * Visually tested by GUI updating with new message, and the correct message being received
+     * by the server.
+     */
+    
+    /**
+     * UPDATE CONVO
+     * Can't be tested without a server. Visually tested. The incoming message from the server
+     * is correctly parsed, added to the Conversation, and displayed in the GUI.
+     */
+    
+    /**
+     * LOGIN
+     * Can't be tested without a server. Testing using print statements, the login message is
+     * properly created, sent, and received by the server.
+     */
+    
+    /**
+     * QUIT
+     * Can't be tested without a server. Visually tested. The login view is opened, all the user's
+     * conversations are closed, and the quit message is correctly generated, sent, and received
+     * by the server.
+     */
+    
+    //Tests setting Online Users
+    @Test
+    public void setOnlineUsersTest() {
+        String username1 = "Banana";
+        String color1 = "yellow";
+        Socket socket1 = new Socket();
+        User user = new User(username1, color1, socket1);
+        ConcurrentHashMap<String, UserInfo> userMap = new ConcurrentHashMap<String, UserInfo>();
+        UserInfo ui = new UserInfo("Blobberty", "orange");
+        userMap.put("Daniel", ui);
+        userMap.put("Marianne", ui);
+        userMap.put("Blobberty", ui);
+        user.setOnlineUsers(userMap);
+        assertEquals(user.getOnlineUsers().keySet(), userMap.keySet());
+    }
+    
+    /**
+     * ADD NEW MY CONVO
+     * Can't be tested without GUI. Tested visually. Conversations are properly created and added
+     * to MyConvos. The GUI is properly updated with the new tab, and the content is filled in the tab.
+     */
+    
+    /**
+     * REMOVE MY CONVO
+     * Can't be tested without GUI. Tested visually. Conversations are properly removed from MyConvos,
+     * and added to the inactiveConvos. The GUI responds correctly, removing the conversation tab, and
+     * saving the history in inactiveConvos so it can be viewed later.
+     */
+    
+    /**
+     * CHECK DUPLICATE CONVO
+     * Can't be tested without GUI. Tested visually. Attempting to create a convo that already exists,
+     * and the appropriate exception in thrown.
+     */
+}
