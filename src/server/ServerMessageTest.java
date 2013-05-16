b5d4f11c462ca52e800dc3cf81ea4cbbd90c0fae
Daniel DiSanto <disanto@mit.edu>
2013-05-16 00:23:02 -0400

    ServerMessage Tests

---
+++ src/server/ServerMessageTest.java
@@ -0,0 +1,38 @@
+package server;
+
+import static org.junit.Assert.*;
+
+import java.net.Socket;
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+public class ServerMessageTest {
+    //Test fetching recipients
+    @Test
+    public void getRecipientsTest() {
+        ArrayList<Socket> recipients = new ArrayList<Socket>();
+        recipients.add(new Socket());
+        recipients.add(new Socket());
+        recipients.add(new Socket());
+        ServerMessage sm = new ServerMessage(recipients, "I love 6.005!");
+        assertEquals(recipients, sm.getRecipients());
+    }
+    
+    //Test fetching Text
+    @Test
+    public void getTextTest() {
+        ArrayList<Socket> recipients = new ArrayList<Socket>();
+        recipients.add(new Socket());
+        recipients.add(new Socket());
+        recipients.add(new Socket());
+        ServerMessage sm = new ServerMessage(recipients, "I love 6.005!");
+        assertEquals("I love 6.005!", sm.getText());
+    }
+    
+    /**
+     * TO STRING
+     * Can't be unit tested without server. Visually verified to work,
+     * method is used only for debug purposes anyway.
+     */
+}
